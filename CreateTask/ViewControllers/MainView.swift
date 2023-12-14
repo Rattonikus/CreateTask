553f285b4cfeb376f9264828d5ad94e4f261748c
Rattonikus <71366308+Rattonikus@users.noreply.github.com>
2023-12-14 08:21:54 -0700

    main view added

--- CreateTask/ViewControllers/MainView.swift
+++ CreateTask/ViewControllers/MainView.swift
@@ -9,14 +9,75 @@ import SwiftUI
 
 struct MainView: View 
 {
-    var body: some View 
+    @State private var backgroundColor : Color = .black
+
+    var body: some View
     {
-        VStack 
+        
+        ZStack 
         {
-            
+            backgroundColor
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack 
+            {
+                HStack
+                {
+                    
+                    Button("1", action: placeholder)
+                        .buttonStyle(.bordered)
+                        .clipShape(Circle())
+                        .frame(maxWidth: .infinity)
+                        .controlSize(.large)
+                        .tint(Color.white)
+                        .font(.system(size: 50))
+                        
+                    Button("2", action: placeholder)
+                        .buttonStyle(.bordered)
+                        .controlSize(.large)
+                        .tint(Color.white)
+                        .clipShape(Circle())
+                        .frame(maxWidth: .infinity)
+                        .font(.system(size: 50))
+
+                    Button("3", action: placeholder)
+                        .buttonStyle(.bordered)
+                        .controlSize(.large)
+                        .tint(Color.white)
+                        .clipShape(Circle())
+                        .frame(maxWidth: .infinity)
+                        .font(.system(size: 50))
+
+                    Button("+", action: placeholder)
+                        .buttonStyle(.bordered)
+                        .background(Color.orange)
+                        .controlSize(.large)
+                        .tint(Color.white)
+                        .clipShape(Circle())
+                        .frame(maxWidth: .infinity)
+                        .font(.system(size: 50))
+
+            }
+                HStack
+                {
+                    Button("0", action: placeholder)
+                        .buttonStyle(.bordered)
+                        .tint(.white)
+                        .clipShape(.capsule)
+                        .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 100)
+                        .font(.system(size: 50))
+
+
+                }
+            }
         }
         
     }
+    
+    func placeholder()
+    {
+        
+    }
+    
 }
 
 #Preview 
